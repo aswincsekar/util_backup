4092d6d97da88475099b867d173600160449046a
aswincsekar <aswincsekar@gmail.com>
2016-11-20 13:19:07 +0530

    Initial commit

--- trigger_probe.py
+++ trigger_probe.py
@@ -22,6 +22,7 @@ def get_metadata(input_file):
                '-show_streams',
                input_file]
     output = sp.check_output(command)
+    output = output.decode('utf-8')
     metadata_output = json.loads(output)
     return metadata_output
 
